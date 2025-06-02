f74a90fa9c4bd319800daae545e1df9489c75015
AadiCJ <acjha990@gmail.com>
2025-06-02 12:20:37 +0530

    Added level switching + minor changed
    Moved the camera and hud into the player instead of making one instance per level.
    added a simple level 2 to check if the game changing logic works.

--- scripts/level_end.gd
+++ scripts/level_end.gd
@@ -1,9 +1,10 @@
 extends Area2D
 
-
+const LEVEL_FOLDER = "res://levels/level_"
 
 func _on_body_entered(_body:Node2D) -> void:
     var currentScene = get_tree().current_scene.scene_file_path
     var nextLevel = currentScene.to_int() + 1
-    print(nextLevel)
-
+    var nextLevelPath = LEVEL_FOLDER + str(nextLevel) + ".tscn"
+    get_tree().change_scene_to_file(nextLevelPath)
+    
