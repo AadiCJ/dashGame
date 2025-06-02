f56404e988cf640cf522c871e0d5df2d4a462c59
AadiCJ <acjha990@gmail.com>
2025-06-02 13:00:34 +0530

    Update killzone.gd
    uhhhh, some formatting issue about tabs and spaces, fixed that

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -2,15 +2,15 @@ extends Area2D
 
 
 func _ready() -> void:
-    #TODO: make dying a signal that is picked up here,
+     #TODO: make dying a signal that is picked up here,
     #but can also be emitted by enemies
     pass
 
 func _on_body_entered(_body:Node2D) -> void:
-	SignalBus.died.emit()
+    SignalBus.died.emit()
     #tell player it has died
-	$Timer.start()
+    $Timer.start()
 
 func _on_timer_timeout() -> void:
-	get_tree().reload_current_scene()
+    get_tree().reload_current_scene()
     #restart after some time
